fb057ac5d94f67a312f89d978439bb7982b4953a
danusorn23456 <40804131+danusorn23456@users.noreply.github.com>
2021-04-24 08:15:58 +0700

    Update scrollArea.js

--- src/components/scrollArea.js
+++ src/components/scrollArea.js
@@ -1,202 +1,212 @@
-import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { createContext, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react'
 
-export default function ScrollArea({ debug, width, height, background, children, className, trackBackground = "white",thumbStyle = {background:"gray"}, size = "12px", ...props }) {
+const ScrollAreaContext = createContext();
 
-    const scrollAreaRef = useRef({});
-    const scrollWrapperRef = useRef({});
+function useScrollAreaContext() {
+    let context = useContext(ScrollAreaContext);
+    if (!context) {
+        return console.error("can't use ScrollAreaContext outside ScrollArea Component")
+    }
+    return context;
+}
 
-    const [isMouseDown, setIsMouseDown] = useState("");
+export default function ScrollArea({ debug, width = '100%', height = "100%", trackWidth = 12, thumbColor = "#242526", trackColor = "#E4E6EB", style, renderRightScrollBar = true, children, className, ...arg }) {
 
-    const scrollBarRef = useRef({
-        right: {
-            el: null,
-            isHover: false,
-            isHidden:false,
-        },
-        bottom: {
-            el: null,
-            isHover: false,
-            isHidden:false,
-        },
-    })
+    //set up some presu-do css without touching your own .css file
+    let styleSheet = document.getElementById("danusorn-custom-scollbar");
 
-    const scrollTrackRef = useRef({
-        right: {
-            el: null,
-        },
-        bottom: {
-            el: null,
-        },
-    });
-
-    const scrollThumbRef = useRef({
-        right: {
-            el: null,
-        },
-        bottom: {
-            el: null,
-        },
-    });
-
-    const [translate, setTranslate] = useState({
-        x: 0,
-        y: 0
-    })
-
-    // scroll bar opacity animate
-    const fadeOutScrollBar = useCallback(() => {
-        if (!scrollBarRef.current.right.isHover) {
-            scrollBarRef.current.right.el.style.opacity = 0;
-            scrollBarRef.current.bottom.el.style.padding = '0px'
+    if (!styleSheet) {
+        styleSheet = document.createElement('style');
+        styleSheet.id = "danusorn-custom-scollbar";
+        styleSheet.innerHTML = `
+        .danusorn-custom-scollbar_remove-scroll-bar::-webkit-scrollbar {
+            display: none;
         }
-        if (!scrollBarRef.current.bottom.isHover) {
-            scrollBarRef.current.bottom.el.style.opacity = 0;
-            scrollBarRef.current.right.el.style.padding = '0px'
+        .danusorn-custom-scollbar_remove-scroll-bar{
+        -ms-overflow-style: none; 
+        scrollbar-width: none;
         }
-    }, [])
-
-    const fadeInScrollBar = useCallback(() => {
-        let scrollArea = scrollAreaRef.current;
-        if (scrollAreaRef.current.scrollHeight - scrollAreaRef.current.offsetHeight > 1) {
-            scrollBarRef.current.right.el.style.opacity = 1;
+        .danusorn-custom-scrollbar_scroll-track:hover{
+            opacity:1 !important;
         }
-        if (scrollAreaRef.current.scrollWidth - scrollAreaRef.current.offsetWidth > 1) {
-            scrollBarRef.current.bottom.el.style.opacity = 1;
-        }
-    }, [])
-
-
-    let stopScrollingTimer;
-
-    function overflowCheck() {
-        // check overflow
-        let isNotOverflowX = scrollAreaRef.current.offsetWidth - scrollAreaRef.current.scrollWidth - (scrollAreaRef.current.offsetWidth - scrollAreaRef.current.clientWidth) == 0;
-        let isNotOverflowY = scrollAreaRef.current.offsetHeight - scrollAreaRef.current.scrollHeight - (scrollAreaRef.current.offsetHeight - scrollAreaRef.current.clientHeight) == 0;
-
-        if (isNotOverflowY) {
-            scrollBarRef.current.right.el.style.display = "none"
-            scrollBarRef.current.right.isHidden = true;
-        } else {
-            scrollBarRef.current.right.el.style.display = "flex"
-            scrollBarRef.current.right.isHidden = false;
-        }
-
-        if (isNotOverflowX){
-            scrollBarRef.current.bottom.el.style.display = "none"
-            scrollBarRef.current.bottom.isHidden = true;
-        } else {
-            scrollBarRef.current.bottom.el.style.display = "flex"
-            scrollBarRef.current.bottom.isHidden = false;
-        }
-
-        if(!scrollBarRef.current.right.isHidden && !scrollBarRef.current.bottom.isHidden){
-            scrollBarRef.current.right.el.style.paddingBottom = size;
-            scrollBarRef.current.bottom.el.style.paddingRight = size;
-        }else{
-            scrollBarRef.current.right.el.style.paddingBottom = '0px';
-            scrollBarRef.current.bottom.el.style.paddingRight = '0px';
-        }
-    
+      `;
+        document.head.appendChild(styleSheet);
     }
 
-    const handleScroll = useCallback(() => {
-        overflowCheck();
-        ajustSize();
-        fadeInScrollBar();
-        
-        //if client stop scrolling 1 sec the fadeout timer will do the job
-        window.clearTimeout(stopScrollingTimer);
-        stopScrollingTimer = setTimeout(fadeOutScrollBar, 1000);
-        
-        let scrollThumb = scrollThumbRef.current;
-        
-        scrollWrapperRef.current.style.top = scrollAreaRef.current.scrollTop +'px';
-        scrollWrapperRef.current.style.left = scrollAreaRef.current.scrollLeft +'px';
-
-        //move scroll thumb to the point
-        scrollThumb.right.el.style.transform = `translateY(${Math.round(scrollAreaRef.current.scrollTop * ((scrollTrackRef.current.right.el.clientHeight - (scrollThumbRef.current.right.el.offsetHeight)) / (scrollAreaRef.current.scrollHeight - scrollAreaRef.current.clientHeight)))}px)`;
-        scrollThumb.bottom.el.style.transform = `translateX(${Math.round(scrollAreaRef.current.scrollLeft * ((scrollTrackRef.current.bottom.el.clientWidth - (scrollThumbRef.current.bottom.el.offsetWidth)) / (scrollAreaRef.current.scrollWidth - scrollAreaRef.current.clientWidth)))}px)`;
-        
-    }, [stopScrollingTimer])
-
-    // about drag the scroll thumb
-    const handleMouseDown = (position) => {
-        setIsMouseDown(position);
+    const variant = {
+        thumbColor: thumbColor,
+        trackColor: trackColor,
+        trackWidth: trackWidth,
     }
 
-    const handleMouseUp = () => {
-        setIsMouseDown("")
+    const [isScrolling, setIsScrolling] = useState(false);
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
+    const [bounding, setBounding] = useState({})
+
+    const scrollArea = useRef({});
+
+    //i just want to scope value that use for calculate some thing
+    const updateBounding = () => {
+        setBounding(prev => {
+            let nextState = { ...prev };
+            nextState.offsetHeight = scrollArea.current.offsetHeight;
+            nextState.offsetWidth = scrollArea.current.offsetWidth;
+            nextState.offsetLeft = scrollArea.current.offsetLeft;
+            nextState.offsetTop = scrollArea.current.offsetTop;
+            nextState.clientTop = scrollArea.current.clientTop;
+            nextState.clientLeft = scrollArea.current.clientLeft;
+            nextState.clientWidth = scrollArea.current.clientWidth;
+            nextState.clientHeight = scrollArea.current.clientHeight;
+            nextState.scrollHeight = scrollArea.current.scrollHeight;
+            nextState.scrollTop = scrollArea.current.scrollTop;
+            nextState.top = scrollArea.current.getBoundingClientRect().top;
+            nextState.left = scrollArea.current.getBoundingClientRect().left;
+            nextState.scrollHeightRemain = scrollArea.current.scrollHeight - scrollArea.current.clientHeight;
+            nextState.trackHeight = scrollArea.current.clientHeight;
+            nextState.trackDragState = scrollArea.current.getBoundingClientRect().height / 2;
+            nextState.trackScrollHeightRemain = (scrollArea.current.getBoundingClientRect().height - Math.max(scrollArea.current.getBoundingClientRect().height) * (scrollArea.current.getBoundingClientRect().height / scrollArea.current.scrollHeight), 100);
+            nextState.ratioHeight = scrollArea.current.clientHeight - Math.max(scrollArea.current.getBoundingClientRect().height * (scrollArea.current.getBoundingClientRect().height / scrollArea.current.scrollHeight), 100);
+            nextState.realY = (e) => (e ? e.clientY - scrollArea.current.getBoundingClientRect().top : null);
+            nextState.buttonHeight = Math.max(scrollArea.current.getBoundingClientRect().height * (scrollArea.current.getBoundingClientRect().height / scrollArea.current.scrollHeight), 100);
+            return nextState;
+        })
     }
 
-    const handleMouseMove = (e) => {
+    let stopScrollTimer;
+
+    function handleAreaScroll() {
+        if (stopScrollTimer) {
+            window.clearTimeout(stopScrollTimer);
+        }
+
+        setIsScrolling(true)
+        updateBounding();
+
+        stopScrollTimer = window.setTimeout(() => setIsScrolling(false), 1000);
+    }
+
+    const handleWindowMouseUp = () => {
+        setIsMouseDown(false);
+    }
+
+    const handleWindowMouseMove = useCallback((e) => {
         if (isMouseDown) {
-            if (isMouseDown === "right") {
-                scrollAreaRef.current.scrollTop = (e.clientY - scrollAreaRef.current.getBoundingClientRect().top - (scrollThumbRef.current.right.el.offsetHeight / 2)) * ((scrollAreaRef.current.scrollHeight - scrollAreaRef.current.clientHeight) / (scrollTrackRef.current.right.el.clientHeight - (scrollThumbRef.current.right.el.offsetHeight)))
-            } else {
-                scrollAreaRef.current.scrollLeft = (e.clientX - scrollAreaRef.current.getBoundingClientRect().left - (scrollThumbRef.current.bottom.el.offsetWidth / 2)) * ((scrollAreaRef.current.scrollWidth - scrollAreaRef.current.clientWidth) / (scrollTrackRef.current.bottom.el.clientWidth - (scrollThumbRef.current.bottom.el.offsetWidth)))
-            }
-        };
-    }
-
-    // prevent scroll thumb fade out when on hover
-    const toggleIsHover = (side) => {
-        scrollBarRef.current[side].isHover = !scrollBarRef.current[side].isHover;
-        if (scrollBarRef.current[side].isHover) {
-            scrollBarRef.current[side].el.style.opacity = 1;
-        } else {
-            fadeOutScrollBar();
+            scrollArea.current.scrollTop = (bounding.realY(e) - (bounding.buttonHeight / 2)) * (bounding.scrollHeightRemain / (bounding.height - bounding.buttonHeight));
         }
+    },[isMouseDown])
+
+    const handleWindowResize = () =>{
+        setBounding(prev => {
+            let nextState = { ...prev };
+            nextState.trackHeight = 0;
+            return nextState;
+        })
     }
 
-    const ajustSize=()=>{
-        scrollThumbRef.current.right.el.style.height = Math.max(100, scrollAreaRef.current.clientHeight - (scrollAreaRef.current.scrollHeight - scrollAreaRef.current.clientHeight)) + 'px';
-        scrollThumbRef.current.bottom.el.style.width = Math.max(100, scrollAreaRef.current.clientWidth - (scrollAreaRef.current.scrollWidth - scrollAreaRef.current.clientWidth)) + 'px';
-    }
+    useEffect(()=>{
+        setBounding(prev => {
+            let nextState = { ...prev };
+            nextState.limitScrollTop = scrollArea.current.scrollHeight - scrollArea.current.clientHeight;
+            return nextState;
+        })
+    },[])
 
     useEffect(() => {
-        if (!scrollAreaRef) return;
+        if (!scrollArea) return;
 
-        overflowCheck();
-        fadeOutScrollBar();
+        let area = scrollArea.current;
 
-        //we need to hide scrollbar immediately for all side then delay the transition
-        setTimeout(() => {
-            scrollBarRef.current.right.el.style.transition = "opacity 250ms";
-            scrollBarRef.current.bottom.el.style.transition = "opacity 250ms";
-        }, 500)
+        updateBounding();
 
-        window.addEventListener('mousemove', handleMouseMove, true);
-        window.addEventListener('mouseup', handleMouseUp);
-        window.addEventListener('resize',handleScroll);
-        return ()=>{
-            window.removeEventListener('mousemove', handleMouseMove, true);
-            window.removeEventListener('mouseup', handleMouseUp);
-            window.removeEventListener('resize',handleScroll);
+        window.addEventListener('resize', handleWindowResize);
+        area.addEventListener('scroll', handleAreaScroll);
+        window.addEventListener('mousemove', handleWindowMouseMove);
+        window.addEventListener('mouseup', handleWindowMouseUp);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+            area.removeEventListener('scroll', handleAreaScroll);
+            window.removeEventListener('mousemove', handleWindowMouseMove);
+            window.removeEventListener('mouseup', handleWindowMouseUp);
+            clearTimeout(stopScrollTimer)
         }
-    }, [])
+
+    }, [isMouseDown])
+
 
     return (
-        <div className={`remove-scroll-bar relative pointer-events-auto ${className}`} style={{ width: width, height: height, background: background, overflow: 'scroll' }} ref={scrollAreaRef} onScroll={handleScroll} onMouseMove={handleMouseMove} >
-            {children}
-            {/* scroll wrapper */}
-            <div ref={scrollWrapperRef} className={`absolute top-0 left-0 w-full h-full ${isMouseDown ? 'pointer-events-auto' : 'pointer-events-none'}`} onMouseUp={handleMouseUp}>
-                {/* right scroll-bar */}
-                <div ref={ref => scrollBarRef.current.right.el = ref} onMouseEnter={() => toggleIsHover("right")} onMouseLeave={() => toggleIsHover("right")} className={`right-0 top-0 h-full absolute pointer-events-auto bt-[200px] border-b[200px] box-content`} style={{width:size, background:trackBackground }}>
-                    {/* right scroll-track */}
-                    <div ref={ref => scrollTrackRef.current.right.el = ref} focus="-1" onMouseDown={() => handleMouseDown("right")} onMouseUp={handleMouseUp} className="h-full w-full flex" style={{ background: trackBackground }}>
-                        {/* right scroll-thumb */}
-                        <button ref={ref => scrollThumbRef.current.right.el = ref} className="w-full h-[100px] rounded-full focus:outline-none"  style={thumbStyle} />
-                    </div>
-                </div>
-                {/* bottom scroll-bar */}
-                <div ref={ref => scrollBarRef.current.bottom.el = ref} onMouseEnter={() => toggleIsHover("bottom")} onMouseLeave={() => toggleIsHover("bottom")} className={`absolute left-0 bottom-0 w-full pointer-events-auto`} style={{ height:size, background:trackBackground }}>
-                    {/* bottom scroll-track */}
-                    <div ref={ref => scrollTrackRef.current.bottom.el = ref} focus="-1" onMouseDown={() => handleMouseDown("bottom")} onMouseUp={handleMouseUp} className="h-full w-full flex" style={{ background: trackBackground }}>
-                        {/* bottom scroll-thumb */}
-                        <button ref={ref => scrollThumbRef.current.bottom.el = ref} className="w-[100px] h-full rounded-full focus:outline-none" style={thumbStyle} />
-                    </div>
-                </div>
+        <ScrollAreaContext.Provider value={{ scrollArea, isScrolling, variant, bounding, isMouseDown, setIsMouseDown }}>
+            <div data-scroll-container ref={scrollArea} style={{ height: height, width: width, position: 'relative', overflow: 'auto', ...style }} className={`danusorn-custom-scollbar_remove-scroll-bar ${className}`} {...arg}>
+                {children}
+                <ScrollHost>
+                    {
+                        renderRightScrollBar && <RightScrollBar />
+                    }
+                </ScrollHost>
             </div>
+        </ScrollAreaContext.Provider>
+    )
+}
+
+const ScrollHost = ({ children }) => {
+    const { bounding, variant } = useScrollAreaContext();
+
+    let scrollHostStyle = useMemo(() => ({
+        display: 'grid',
+        position: 'absolute',
+        width: bounding.clientWidth,
+        top: bounding.scrollTop,
+        overflow:'visible',
+        left:0,
+        zIndex:9999999,
+        gridTemplateColumns: `1fr ${variant.trackWidth}px`,
+        gridTemplateRows: 'empty rightScroll',
+        pointerEvents: 'none',
+        opacity: 0.5,
+    }), [variant, bounding])
+
+    return (
+        <div data-scroll-host style={scrollHostStyle}>
+            {children}
+        </div>
+    )
+}
+
+const RightScrollBar = () => {
+
+    const { variant, bounding, isScrolling, setIsMouseDown } = useScrollAreaContext();
+
+    const scrollTrack = useRef({});
+    const scrollButton = useRef({});
+
+    let trackStyle = useMemo(() => ({
+        marginTop:0,
+        padding: '0px 2px',
+        pointerEvents: 'auto',
+        background: variant.trackColor,
+        transition: 'opacity 200ms',
+        opacity: isScrolling ? 0.6 : 0,
+        height: bounding.trackHeight,
+        display: bounding.scrollHeight - bounding.clientHeight < 1 ? 'none' : 'block',
+        width: variant.trackWidth,
+        overflow: 'hidden',
+        gridArea: 'rightScroll',
+    }), [isScrolling, variant, bounding])
+
+    const buttonStyle = useMemo(() => ({
+        zIndex:'999999',
+        width: '100%',
+        outline: 'none',
+        background: variant.thumbColor,
+        borderRadius: variant.trackWidth,
+        height: bounding.buttonHeight,
+        transform: `translateY(${bounding.scrollTop * (bounding.ratioHeight / bounding.scrollHeightRemain)}px)`,
+    }), [variant, bounding])
+
+    return (
+        <div data-scroll-track ref={scrollTrack} onMouseDown={() => setIsMouseDown(true)} onMouseUp={() => setIsMouseDown(false)} style={trackStyle} className="danusorn-custom-scrollbar_scroll-track">
+            <button ref={scrollButton} style={buttonStyle}></button>
         </div>
     )
 }
