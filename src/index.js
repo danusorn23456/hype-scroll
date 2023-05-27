743f42fd42234af3c6e76fbc6722f5158036dff3
danusorn23456 <danusorn23456@gmail.com>
2023-05-28 03:15:07 +0700

    fixing index.css not found

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
 
