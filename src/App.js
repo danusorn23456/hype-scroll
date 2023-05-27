aba180f8d8a4138873969665c81301c8d7bf475d
danusorn23456 <danusorn23456@gmail.com>
2023-05-28 02:41:31 +0700

    refactor animation and remove unsed depinency

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
 import React from 'react';
-import ScrollArea from './components/scrollArea';
+import { ScrollArea } from './components';
+
+
+const titleStyle = {
+  color: 'white',
+}
+
+const rootStyle = {
+  height: '100vh',
+  width: '100vw',
+  background: '#202020',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const scrollAreStyle = {
+  position: 'relative',
+  width: 400,
+  height: 400,
+  margin: '0 auto',
+  padding: 24,
+  borderRadius: '1rem',
+  background: 'rgba(255,255,255,0.1)',
+  border: '1px solid white',
+  backdropFilter: 'blur(2px)',
+}
+
+const mockBoxStyle = {
+  height: 1000,
+  background: 'white',
+  padding: 16,
+}
 
 function App() {
 
   return (
     <>
-      <ScrollArea className="!h-screen flex flex-col bg-[#F1F9FB]">
-        <div className="container mx-auto">
-          <header className="py-16">
-            <h1 className="text-6xl font-medium text-center">AUTO HIDE SCROLL BAR</h1>
-          </header>
-          <div className="flex py-8 space-x-8">
-            <h1>Y</h1>
-            <ScrollArea scrollY className={`!w-1/3 !h-96 bg-white border p-10 mx-auto relative`}>
-              <p className="">
-                Excepteur laborum adipisicing nostrud aliqua ea laborum non eiusmod id pariatur. Est nulla pariatur cupidatat laboris ipsum laboris irure deserunt. Labore minim dolore sint in anim veniam excepteur elit pariatur mollit proident do exercitation. Proident ipsum consectetur aliqua mollit voluptate quis ipsum voluptate nostrud pariatur exercitation pariatur. Eiusmod occaecat pariatur velit fugiat deserunt culpa magna dolore incididunt ut.
-                Deserunt incididunt ad cupidatat dolor laborum excepteur elit quis tempor dolor aliquip qui. Reprehenderit culpa irure minim do officia nisi occaecat tempor. Id ex quis commodo non eiusmod adipisicing exercitation non velit reprehenderit non nulla. Commodo esse reprehenderit do sint dolore tempor amet. Id aliqua consequat duis anim culpa aliqua.
-                Velit do cillum consectetur laborum enim et reprehenderit ad officia occaecat esse officia et deserunt. Et eu deserunt est duis proident aliqua fugiat laborum proident reprehenderit. Occaecat fugiat eiusmod mollit nostrud. Fugiat do consequat reprehenderit ipsum veniam amet minim nisi.
-                Minim sunt elit ullamco dolor aliqua irure qui qui minim mollit ea duis ex. Amet consectetur quis sint amet veniam consectetur excepteur exercitation dolore eu. Commodo reprehenderit ipsum ex eiusmod cillum. Est adipisicing irure enim sit incididunt nostrud qui magna. Qui esse in et velit quis.
-                Eu esse Lorem Lorem nulla minim eiusmod. Reprehenderit nostrud nostrud Lorem consectetur ad. Incididunt cupidatat cillum elit sunt Lorem consectetur et Lorem fugiat deserunt. Est magna elit est reprehenderit. Ad veniam dolore culpa adipisicing exercitation sit.
-                Anim aliqua id consectetur consequat anim anim anim ut aliquip sit ad nostrud ex officia. Pariatur non laborum sit est ipsum. Nulla elit ipsum minim esse incididunt consectetur qui laborum labore voluptate ullamco id aute nulla. Ullamco amet dolor aliquip sunt non laborum.
-                Culpa exercitation tempor fugiat minim tempor veniam exercitation dolor do nisi. Reprehenderit adipisicing et sunt amet aliquip do esse cupidatat laborum minim id amet sunt enim. Do cillum ut sunt sunt sint ex cupidatat. Eu id dolor laboris amet.
-                Incididunt magna minim dolor quis excepteur consequat et reprehenderit consectetur. Labore dolor duis sit aute mollit nostrud dolore elit aliqua. In do dolore et nostrud proident labore nisi cupidatat. Quis ut incididunt ullamco in in. Veniam in minim tempor ex excepteur laborum id ipsum aliqua. Irure tempor proident tempor ullamco ex officia ullamco do.
-                Nulla eu sint aute et incididunt veniam. In laboris mollit duis pariatur ex occaecat est officia. Mollit id officia nulla anim cupidatat elit occaecat in reprehenderit qui incididunt est. Ea mollit commodo laborum aliquip.
-                Commodo nostrud aute eu exercitation ipsum adipisicing irure ullamco. Elit eu labore enim deserunt ullamco elit aute. Ex culpa do excepteur cillum aliqua et ut ipsum anim ullamco minim cillum deserunt. Ut minim quis ipsum excepteur est irure aliquip nostrud aliqua ut sunt. Ipsum fugiat aute adipisicing sunt officia magna excepteur mollit.
-              </p>
-            </ScrollArea>
-            <h1>X</h1>
-            <ScrollArea scrollX scrollY={false} className={`!w-1/3 !h-96 border bg-white p-10 mx-auto`}>
-              <p className="w-[200%]">
-                Excepteur laborum adipisicing nostrud aliqua ea laborum non eiusmod id pariatur. Est nulla pariatur cupidatat laboris ipsum laboris irure deserunt. Labore minim dolore sint in anim veniam excepteur elit pariatur mollit proident do exercitation. Proident ipsum consectetur aliqua mollit voluptate quis ipsum voluptate nostrud pariatur exercitation pariatur. Eiusmod occaecat pariatur velit fugiat deserunt culpa magna dolore incididunt ut.
-                Deserunt incididunt ad cupidatat dolor laborum excepteur elit quis tempor dolor aliquip qui. Reprehenderit culpa irure minim do officia nisi occaecat tempor. Id ex quis commodo non eiusmod adipisicing exercitation non velit reprehenderit non nulla. Commodo esse reprehenderit do sint dolore tempor amet. Id aliqua consequat duis anim culpa aliqua.
-                Velit do cillum consectetur laborum enim et reprehenderit ad officia occaecat esse officia et deserunt. Et eu deserunt est duis proident aliqua fugiat laborum proident reprehenderit. Occaecat fugiat eiusmod mollit nostrud. Fugiat do consequat reprehenderit ipsum veniam amet minim nisi.
-                Minim sunt elit ullamco dolor aliqua irure qui qui minim mollit ea duis ex. Amet consectetur quis sint amet veniam consectetur excepteur exercitation dolore eu. Commodo reprehenderit ipsum ex eiusmod cillum. Est adipisicing irure enim sit incididunt nostrud qui magna. Qui esse in et velit quis.
-                Eu esse Lorem Lorem nulla minim eiusmod. Reprehenderit nostrud nostrud Lorem consectetur ad. Incididunt cupidatat cillum elit sunt Lorem consectetur et Lorem fugiat deserunt. Est magna elit est reprehenderit. Ad veniam dolore culpa adipisicing exercitation sit.
-                Anim aliqua id consectetur consequat anim anim anim ut aliquip sit ad nostrud ex officia. Pariatur non laborum sit est ipsum. Nulla elit ipsum minim esse incididunt consectetur qui laborum labore voluptate ullamco id aute nulla. Ullamco amet dolor aliquip sunt non laborum.
-                Culpa exercitation tempor fugiat minim tempor veniam exercitation dolor do nisi. Reprehenderit adipisicing et sunt amet aliquip do esse cupidatat laborum minim id amet sunt enim. Do cillum ut sunt sunt sint ex cupidatat. Eu id dolor laboris amet.
-                Incididunt magna minim dolor quis excepteur consequat et reprehenderit consectetur. Labore dolor duis sit aute mollit nostrud dolore elit aliqua. In do dolore et nostrud proident labore nisi cupidatat. Quis ut incididunt ullamco in in. Veniam in minim tempor ex excepteur laborum id ipsum aliqua. Irure tempor proident tempor ullamco ex officia ullamco do.
-                Nulla eu sint aute et incididunt veniam. In laboris mollit duis pariatur ex occaecat est officia. Mollit id officia nulla anim cupidatat elit occaecat in reprehenderit qui incididunt est. Ea mollit commodo laborum aliquip.
-                Commodo nostrud aute eu exercitation ipsum adipisicing irure ullamco. Elit eu labore enim deserunt ullamco elit aute. Ex culpa do excepteur cillum aliqua et ut ipsum anim ullamco minim cillum deserunt. Ut minim quis ipsum excepteur est irure aliquip nostrud aliqua ut sunt. Ipsum fugiat aute adipisicing sunt officia magna excepteur mollit.
-              </p>
-            </ScrollArea>
-            <h1>XY</h1>
-            <ScrollArea scrollX scrollY className={`!w-1/3 !h-96 border bg-white p-10 mx-auto relative`}>
-              <p className="w-[120%]">
-                Excepteur laborum adipisicing nostrud aliqua ea laborum non eiusmod id pariatur. Est nulla pariatur cupidatat laboris ipsum laboris irure deserunt. Labore minim dolore sint in anim veniam excepteur elit pariatur mollit proident do exercitation. Proident ipsum consectetur aliqua mollit voluptate quis ipsum voluptate nostrud pariatur exercitation pariatur. Eiusmod occaecat pariatur velit fugiat deserunt culpa magna dolore incididunt ut.
-                Deserunt incididunt ad cupidatat dolor laborum excepteur elit quis tempor dolor aliquip qui. Reprehenderit culpa irure minim do officia nisi occaecat tempor. Id ex quis commodo non eiusmod adipisicing exercitation non velit reprehenderit non nulla. Commodo esse reprehenderit do sint dolore tempor amet. Id aliqua consequat duis anim culpa aliqua.
-                Velit do cillum consectetur laborum enim et reprehenderit ad officia occaecat esse officia et deserunt. Et eu deserunt est duis proident aliqua fugiat laborum proident reprehenderit. Occaecat fugiat eiusmod mollit nostrud. Fugiat do consequat reprehenderit ipsum veniam amet minim nisi.
-                Minim sunt elit ullamco dolor aliqua irure qui qui minim mollit ea duis ex. Amet consectetur quis sint amet veniam consectetur excepteur exercitation dolore eu. Commodo reprehenderit ipsum ex eiusmod cillum. Est adipisicing irure enim sit incididunt nostrud qui magna. Qui esse in et velit quis.
-                Eu esse Lorem Lorem nulla minim eiusmod. Reprehenderit nostrud nostrud Lorem consectetur ad. Incididunt cupidatat cillum elit sunt Lorem consectetur et Lorem fugiat deserunt. Est magna elit est reprehenderit. Ad veniam dolore culpa adipisicing exercitation sit.
-                Anim aliqua id consectetur consequat anim anim anim ut aliquip sit ad nostrud ex officia. Pariatur non laborum sit est ipsum. Nulla elit ipsum minim esse incididunt consectetur qui laborum labore voluptate ullamco id aute nulla. Ullamco amet dolor aliquip sunt non laborum.
-                Culpa exercitation tempor fugiat minim tempor veniam exercitation dolor do nisi. Reprehenderit adipisicing et sunt amet aliquip do esse cupidatat laborum minim id amet sunt enim. Do cillum ut sunt sunt sint ex cupidatat. Eu id dolor laboris amet.
-                Incididunt magna minim dolor quis excepteur consequat et reprehenderit consectetur. Labore dolor duis sit aute mollit nostrud dolore elit aliqua. In do dolore et nostrud proident labore nisi cupidatat. Quis ut incididunt ullamco in in. Veniam in minim tempor ex excepteur laborum id ipsum aliqua. Irure tempor proident tempor ullamco ex officia ullamco do.
-                Nulla eu sint aute et incididunt veniam. In laboris mollit duis pariatur ex occaecat est officia. Mollit id officia nulla anim cupidatat elit occaecat in reprehenderit qui incididunt est. Ea mollit commodo laborum aliquip.
-                Commodo nostrud aute eu exercitation ipsum adipisicing irure ullamco. Elit eu labore enim deserunt ullamco elit aute. Ex culpa do excepteur cillum aliqua et ut ipsum anim ullamco minim cillum deserunt. Ut minim quis ipsum excepteur est irure aliquip nostrud aliqua ut sunt. Ipsum fugiat aute adipisicing sunt officia magna excepteur mollit.
-              </p>
-            </ScrollArea>
-            
-          </div>
-        </div>
+      <ScrollArea style={rootStyle}>
+        <h2 style={titleStyle}>HYPE SCROLL</h2>
+        <ScrollArea scrollY scrollX thumbColor='linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 54%)' style={scrollAreStyle}>
+          <p style={mockBoxStyle}>
+            The AutoHide Scrollbar project is a custom React component
+            designed to enhance the user experience of scrolling within a web application.
+            Unlike the default scrollbars, this component offers auto-hiding functionality,
+            which means the scrollbar remains hidden until the user hovers over it,
+            providing a cleaner and more immersive interface.
+          </p>
+        </ScrollArea>
       </ScrollArea>
     </>
 
--- src/components/scroll-area.js
+++ src/components/scroll-area.js
@@ -1,10 +1,9 @@
-import React, {  useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 
-
-export default function ScrollArea({ as, width = '100%', height = "100%", trackSize = 16, thumbColor = "#242526", trackColor = "#E4E6EB", style, scrollX, scrollY = true, children, className, ...arg }) {
+function ScrollArea({ as, width = '100%', height = "100%", trackSize = 14, thumbColor = "white", trackColor = "rgba(20,20,20,0.8)", style, scrollX, scrollY = true, autoHideDuration = 1000, children, className, ...arg }) {
 
     //set up some presu-do css without touching your own .css file
-    let prefix = "danusorn-custom-scollbar"
+    let prefix = "hype-scollbar"
     let styleSheet = document.getElementById(prefix);
     if (!styleSheet) {
         styleSheet = document.createElement('style');
@@ -14,8 +13,8 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
             display: none;
         }
         .${prefix}_remove-scroll-bar{
-        -ms-overflow-style: none; 
-        scrollbar-width: none;
+            -ms-overflow-style: none; 
+            scrollbar-width: none;
         }
         .${prefix}_scroll-track:hover{
             opacity:0.6 !important;
@@ -34,14 +33,14 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
         buttonMargin: Math.round(trackSize / 4),
     }
 
-    const [isScrolling, setIsScrolling] = useState(false);
-    const [isMouseDown, setIsMouseDown] = useState(false);
-
+    const mouseRef = useRef({
+        mouseDown: false,
+        scrollBarPosition: ""
+    })
     const scrollArea = useRef({});
     const scrollHost = useRef({});
     const scrollBar = useRef({});
 
-    const raf = useRef(null);
     const stopScrollTimer = useRef(null);
 
     //default inline style without any calculate logic
@@ -49,6 +48,7 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
         area: {
             height: height,
             width: width,
+            transform: 'translateZ(0)',
             overflowX: scrollX ? 'auto' : 'hidden',
             overflowY: scrollY ? 'auto' : 'hidden',
             ...style
@@ -56,38 +56,39 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
         host: {
             transitionDuration: 0,
             position: 'fixed',
-            top:0,
-            left:0,
+            top: 0,
+            left: 0,
             pointerEvents: 'none',
             willChange: 'transform',
             overflow: 'hidden',
             zIndex: 999999,
         },
         track: {
-            transformOrigin:'center',
-            opacity: isScrolling ? 0.6 : 0,
+            opacity: 0,
+            transformOrigin: 'center',
             background: trackColor,
             pointerEvents: 'auto',
             transition: 'opacity 300ms',
+            borderRadius: 'circle',
         },
         button: {
+            cusor: "pointer",
             transition: 'transform 50ms',
             willChange: 'transform',
             background: thumbColor,
             outline: 'none',
+            borderStyle: 'none',
             borderRadius: trackSize,
         }
     }
     const animate = useCallback(() => {
-
         let area = scrollArea.current;
-        if(!area)return;
+        if (!area) return;
 
         area.scrollTopLimit = area.scrollHeight - area.clientHeight;
         area.scrollLeftLimit = area.scrollWidth - area.clientWidth;
         area.isOverflowY = scrollY && area.scrollHeight - area.clientHeight > 1;
-        area.isOverflowX =  scrollX && area.scrollWidth - area.clientWidth > 1;
-        area.hasTransform = window.getComputedStyle(scrollArea.current).getPropertyValue('transform') !== "none";
+        area.isOverflowX = scrollX && area.scrollWidth - area.clientWidth > 1;
 
         let rightScrollBar = scrollBar.current.right;
         let bottomScrollBar = scrollBar.current.bottom;
@@ -97,7 +98,8 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
 
         let host = scrollHost.current;
         area.style.zIndex = window.getComputedStyle(area).getPropertyValue('position') === "fixed" ? 1 : 0;
-        host.style.transform = area.hasTransform ? `translate(${area.scrollLeft}px,${area.scrollTop}px)` : `translate(${area.getBoundingClientRect().left + area.clientLeft}px,${area.getBoundingClientRect().top + area.clientTop}px)`;
+
+        host.style.transform = `translate(${area.scrollLeft}px,${area.scrollTop}px)`
         host.style.width = area.clientWidth + 'px';
         host.style.height = area.clientHeight + 'px';
 
@@ -106,13 +108,13 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
             button = rightScrollBar.children[0];
 
             track.style.width = trackSize + 'px';
-            track.style.height = area.isOverflowX ? area.clientHeight - trackSize+'px' : area.clientHeight+'px';
+            track.style.height = area.isOverflowX ? area.clientHeight - trackSize + 'px' : area.clientHeight + 'px';
             track.style.display = area.isOverflowY ? 'flex' : 'none';
             track.style.padding = `0 ${Math.floor(variant.buttonMargin)}px`;
             track.style.marginLeft = 'auto';
 
             button.style.width = '100%';
-            button.style.height = area.clientHeight < 60 ? area.clientHeight * (area.clientHeight / area.scrollHeight) : Math.max(area.clientHeight * (area.clientHeight / area.scrollHeight), 50)+'px';
+            button.style.height = area.clientHeight < 60 ? area.clientHeight * (area.clientHeight / area.scrollHeight) : Math.max(area.clientHeight * (area.clientHeight / area.scrollHeight), 50) + 'px';
             button.style.transform = `scaleY(0.${Math.round(100 - 3.5)}) translateY(${(area.scrollTop) * ((track.offsetHeight - button.offsetHeight) / area.scrollTopLimit) + variant.buttonMargin / 2}px)`
         }
 
@@ -121,82 +123,102 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
             button = bottomScrollBar.children[0];
 
             track.style.height = trackSize + 'px';
-            track.style.width = area.isOverflowY ? area.clientWidth - trackSize+'px' : area.clientWidth+'px';
+            track.style.width = area.isOverflowY ? area.clientWidth - trackSize + 'px' : area.clientWidth + 'px';
             track.style.display = area.isOverflowX ? 'flex' : 'none';
             track.style.padding = `${Math.floor(variant.buttonMargin)}px 0`;
-            track.style.marginTop = area.isOverflowY ? 0 : area.clientHeight - trackSize+'px';
+            track.style.marginTop = area.isOverflowY ? 0 : area.clientHeight - trackSize + 'px';
 
             button.style.height = '100%';
-            button.style.width = area.clientWidth < 60 ? area.clientWidth * (area.clientWidth / area.scrollWidth) : Math.max(area.clientWidth * (area.clientWidth / area.scrollWidth), 50)+'px';
+            button.style.width = area.clientWidth < 60 ? area.clientWidth * (area.clientWidth / area.scrollWidth) : Math.max(area.clientWidth * (area.clientWidth / area.scrollWidth), 50) + 'px';
             button.style.transform = `scaleX(0.${Math.round(100 - variant.buttonMargin / 2)}) translate(${(area.scrollLeft) * ((track.offsetWidth - button.offsetWidth) / area.scrollLeftLimit) + variant.buttonMargin / 2}px)`;
         }
 
-        raf.current = requestAnimationFrame(animate);
-    },[scrollX, scrollY, trackSize, variant.buttonMargin])
+        const scrollBars = Object.values(scrollBar.current)
 
-    const handleAreaScroll = () => {
+        if (scrollBars) {
+            scrollBars.forEach(bar => {
+                bar.style.opacity = 1
+            })
+        }
+
         if (stopScrollTimer.current) {
             window.clearTimeout(stopScrollTimer.current);
         }
-        setIsScrolling(true)
-        stopScrollTimer.current = window.setTimeout(() => setIsScrolling(false), 1000);
+
+        stopScrollTimer.current = window.setTimeout(() => {
+            scrollBars.forEach(scroll => {
+                scroll.style.opacity = 0
+            })
+        }, autoHideDuration);
+
+    }, [scrollX, scrollY, trackSize, variant.buttonMargin, autoHideDuration])
+
+    const handleAreaScroll = () => {
+        animate()
     }
 
     const handleWindowMouseMove = useCallback(e => {
-        e.preventDefault();
+        let scrollBarPosition = mouseRef.current.scrollBarPosition
         let area = scrollArea.current;
         let rightScrollBar = scrollBar.current.right;
         let bottomScrollBar = scrollBar.current.bottom;
-        
+
         let button;
         let track;
+        const isMouseDown = mouseRef.current.mouseDown
 
         if (isMouseDown) {
-            if(scrollY){
+            if (scrollY && scrollBarPosition === "right") {
                 track = rightScrollBar;
                 button = rightScrollBar.children[0];
                 scrollArea.current.scrollTop = ((e.clientY - area.getBoundingClientRect().top) - (button.offsetHeight / 2)) * (area.scrollTopLimit / (track.offsetHeight - button.offsetHeight));
             }
-            if(scrollX){
+            if (scrollX && scrollBarPosition === "bottom") {
                 track = bottomScrollBar;
                 button = bottomScrollBar.children[0];
                 scrollArea.current.scrollLeft = ((e.clientX - area.getBoundingClientRect().left) - (button.offsetWidth / 2)) * (area.scrollLeftLimit / (track.offsetWidth - button.offsetWidth));
             }
         }
-    },[isMouseDown,scrollX,scrollY]);
+    }, [scrollX, scrollY]);
 
     const handleWindowMouseUp = () => {
-        setIsMouseDown(false);
+        mouseRef.current = {
+            ...mouseRef.current,
+            mouseDown: false
+        }
     }
 
-    const handleTrackMouseDown = () =>{
-        setIsMouseDown(true);
+    const handleTrackMouseDown = (scrollBarPosition) => {
+        mouseRef.current = {
+            ...mouseRef.current,
+            scrollBarPosition,
+            mouseDown: true
+        }
     }
 
     useEffect(() => {
         window.addEventListener('mousemove', handleWindowMouseMove);
         window.addEventListener('mouseup', handleWindowMouseUp);
-        raf.current = requestAnimationFrame(animate);
+        animate()
 
         return () => {
             window.removeEventListener('mousemove', handleWindowMouseMove);
             window.removeEventListener('mouseup', handleWindowMouseUp);
-            cancelAnimationFrame(raf.current);
             window.clearTimeout(stopScrollTimer.current);
         }
-    },[handleWindowMouseMove,animate])
+    }, [handleWindowMouseMove, animate])
 
     return (
         <TAG ref={scrollArea} style={inlineStyle.area} onScroll={handleAreaScroll} className={`${prefix}_remove-scroll-bar ${className}`} {...arg}>
             {children}
             <div className={`${prefix}_scroll-host`} ref={scrollHost} style={inlineStyle.host}>
                 {scrollY && (
-                    <div className={`${prefix}_scroll-track`} ref={ref => scrollBar.current.right = ref} onMouseDown={handleTrackMouseDown}  style={inlineStyle.track}>
+                    <div data-scroll-bar={"right"} className={`${prefix}_scroll-track`} ref={ref => scrollBar.current.right = ref} onMouseDown={e => handleTrackMouseDown("right")} style={inlineStyle.track}>
                         <button style={inlineStyle.button}></button>
                     </div>)
                 }
                 {scrollX && (
-                    <div className={`${prefix}_scroll-track`} ref={ref => scrollBar.current.bottom = ref} onMouseDown={handleTrackMouseDown} style={inlineStyle.track}>
+                    <div data-scroll-bar={"bottom"} className={`${prefix}_scroll-track`} ref={ref => scrollBar.current.bottom = ref} onMouseDown={e => handleTrackMouseDown("bottom")} style={inlineStyle.track}>
                         <button style={inlineStyle.button}></button>
                     </div>)
                 }
@@ -205,4 +227,5 @@ export default function ScrollArea({ as, width = '100%', height = "100%", trackS
     )
 }
 
+export { ScrollArea }
 
